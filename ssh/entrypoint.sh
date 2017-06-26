55d17350581505a1d6b07028d4f9fa6ea2b98fd6
auzias <github@auzias.net>
2017-06-26 11:30:35 +0200

    Entrypoint displays fingerprints
    Signed-off-by: auzias <github@auzias.net>

---
+++ ssh/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Displays fingerprints of public keys (to verify the host)
+for f in /etc/ssh/*pub; do
+    ssh-keygen -l -E md5 -f $f
+    ssh-keygen -l -f $f
+done
+
+# Runs the daemon
+/usr/sbin/sshd -D
