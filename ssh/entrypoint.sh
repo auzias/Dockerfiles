518196fd919c8abe8b1b10e15d4dc90cf480b692
auzias <github@auzias.net>
2017-06-29 11:59:45 +0200

    Heimdall (sshd) working. Ready to be tested for use-case.

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+if [ -z $UID ]
+  then
+    echo "No UID arguments supplied for Heimdall user."
+    echo "To set it to your current, use the docker-run option:"
+    echo '`-e UID=\$(id|cut -d "=" -f 2|cut -d "(" -f 1)`'
+    exit
+fi
+
+usermod -u $UID Heimdall
+
 # Displays fingerprints of public keys (to verify the host)
 for f in /etc/ssh/*pub; do
     ssh-keygen -l -E md5 -f $f
     ssh-keygen -l -f $f
 done
+
+ssh -V
 
 # Runs the daemon
 /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
