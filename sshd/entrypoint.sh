43b251f1755634954dd79c249676542238528fed
Mael <git@azs.email>
2020-05-14 21:40:23 +0200

    [sshd] Cleaner, simpler, safer `Dockerfile`, `entrypoint.sh` and `sshd_config` for sshd image

--- sshd/entrypoint.sh
+++ sshd/entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [ -z $UID ]
-  then
-    echo "No UID arguments supplied for Heimdall user."
-    echo "To set it to your current, use the docker-run option:"
-    echo '`-e UID=\$(id|cut -d "=" -f 2|cut -d "(" -f 1)`'
-    exit
+if [ -z $UID ] ; then
+    echo "Default UID=1000 is kept for the user Heimdall"
+    echo "If an error message as follow appears:"
+    echo ">  Authentication refused: bad ownership or modes for file /home/Heimdall/.ssh/authorized_keys"
+    echo "then you shall pass the UID of the owner of the file `authorized_key` mounted."
+else
+    usermod -u $UID Heimdall
 fi
 
-usermod -u $UID Heimdall
-
-# Displays fingerprints of public keys (to verify the host)
+# Prints fingerprints of public keys (to verify the host)
 for f in /etc/ssh/*pub; do
     ssh-keygen -l -E md5 -f $f
     ssh-keygen -l -f $f
 done
 
+# Prints sshd version before starting
 ssh -V
 
 # Runs the daemon
